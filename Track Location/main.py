38289fa21936910e7a92a6d28eb14bb8cfb70463
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-07 13:12:51 +0530

    updated number

--- Track Location/main.py
+++ Track Location/main.py
@@ -1,4 +1,6 @@
 import phonenumbers
+import opencage 
+
 from myphone import number
 
 from phonenumbers import geocoder 
@@ -11,3 +13,18 @@ print(location)
 from phonenumbers import carrier
 service_pro = phonenumbers.parse(number)
 print(carrier.name_for_number(service_pro, "en"))
+
+from opencage.geocoder import OpenCageGeocode
+
+
+key = 'd010cd969df64c48a2a0b0560601da1d'
+
+geocoder = OpenCageGeocode(key)
+query = str(location)
+results = geocoder.geocode(query)
+print(results) 
+
+lat = results[0]['geometry']['lat']
+lng = results[0]['geometry']['lng']
+ 
+print(lat,lng)
